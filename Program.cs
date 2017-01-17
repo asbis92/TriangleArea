41d5553a0d89c79c004a18251a78672d1c8d7222
asbis92 <rdsl_ivanov@yahoo.com>
2017-01-17 16:35:00 +0200

    A simple console problem that calculates the triangle area

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleAreaMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            Console.WriteLine(TriangleArea(a, h));
+        }
+        static double TriangleArea(double a, double h)
+        {
+            double area = a * h / 2;
+            return area;
+        }
+    }
+}
